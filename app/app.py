f62f5308b945595b5869251991e42e84cfca723e
ChristinaZXY <zhangxiaoyusg@gmail.com>
2018-06-14 22:00:50 -0400

    2018-06-14 commit

--- app/app.py
+++ app/app.py
@@ -1,49 +1,102 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash, url_for
+from flask_debugtoolbar import DebugToolbarExtension
 try: import simplejson as json
 except ImportError: import json
 import datetime
+from calendar import monthrange
 import requests
 import pandas
 import io
-import bokeh
-#from bokeh.plotting import figure
-#from bokeh.resources import CDN
-#from bokeh.embed import file_html
-
+from bokeh.plotting import figure, output_file, show
+from bokeh.resources import CDN
+from bokeh.embed import components
+from wtforms import validators, StringField, BooleanField, SubmitField, widgets, SelectMultipleField
+from flask_wtf import FlaskForm
+from flask_nav import Nav
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+#app.config.from_object(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'webSafetyPurpose'
+Bootstrap(app)
+nav = Nav(app)
+toolbar = DebugToolbarExtension(app)
 
-@app.route('/')
-def my_form():
-    return render_template('input_page.html')
+class inputForm(FlaskForm):
+    ticker = StringField('ticker', validators=[validators.required()])
+    year = StringField('year', validators=[validators.required()])
+    month = StringField('month', validators=[validators.required()])
+    line1 = BooleanField('line1')
+    line2 = BooleanField('line2')
+    line3 = BooleanField('line3')
+    line4 = BooleanField('line4')
 
-@app.route('/', methods=['POST'])
-def my_form_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
+@app.route('/', methods=['POST','GET'])
+def index():
+    form = inputForm()
 
-# accepts a stock ticker input from the user and plots closing price data for the last month
-ticker = 'FB'
-today = datetime.date.today()
-firstdaythismonth = today.replace(day=1)
-lastdaylastmonth = firstdaythismonth - datetime.timedelta(days=1)
-onemonthago = lastdaylastmonth.replace(day=today.day)
+    if form.validate_on_submit():
+        ticker = form.ticker.data
+        ticker = ticker.upper()
+        year = form.year.data
+        month = form.month.data
+        lines = []
+        if form.line1.data:
+            lines.append('Close')
+        if form.line2.data:
+            lines.append('Adj. Close')
+        if form.line3.data:
+            lines.append('Open')
+        if form.line4.data:
+            lines.append('Adj. Open')
+        return redirect(url_for('graph', ticker=ticker, year=year, month=month, lines=lines))
+    else:
+        print(form.errors)
 
-payload = {'start_date' : onemonthago, 'end_date': today, 'api_key': 'HZpg_ZpjP46cpex3eccE'}
-r = requests.get('https://www.quandl.com/api/v3/datasets/WIKI/' + ticker + '/data.csv', params=payload)
-df = pandas.read_csv(io.StringIO(r.text))
+    return render_template('input_page.html', form=form)
 
-#plot = bokeh.plotting.figure()
-#plot.circle([1,2], [3,4])
-#htmlpage = bokeh.embed.file_html(plot, bokeh.resources.CDN, "my plot")
 
-#@app.route('/')
-#def index():
-#  return render_template('index.html')
+@app.route('/graph/<ticker>/<year>/<month>/<lines>') # /<ticker>/<year>/<month>/<lines>
+def graph(ticker, year, month, lines): # ticker, year, month, lines
 
+    # ticker = request.args.get('ticker')
+    # year = int(request.args.get('year'))
+    # month = int(request.args.get('month'))
+    # lines = request.args.get('lines')
+
+    # today = datetime.date.today()
+    # firstdaythismonth = today.replace(day=1)
+    # lastdaylastmonth = firstdaythismonth - datetime.timedelta(days=1)
+    # onemonthago = lastdaylastmonth.replace(day=today.day)
+
+    year = int(year)
+    month = int(month)
+    datestart = datetime.date(year, month, 1)
+    dateend = datetime.date(year, month, monthrange(year, month)[1])
+
+    payload = {'start_date': datestart, 'end_date': dateend, 'api_key': 'HZpg_ZpjP46cpex3eccE'}
+    r = requests.get('https://www.quandl.com/api/v3/datasets/WIKI/' + ticker + '/data.csv', params=payload)
+    df = pandas.read_csv(io.StringIO(r.text), parse_dates=['Date'])
+
+    Colors = {'Close': 'navy', 'Adj. Close': 'cyan', 'Open': 'firebrick', 'Adj. Open': 'salmon'}
+    p = figure(x_axis_type="datetime", plot_width=800, plot_height=500)
+    lines = eval(lines) # convert the string form of a list back to a list
+    for line in lines:
+        p.line(x=df['Date'], y=df[line], color=Colors[line], legend=line, line_width=2)
+    p.title.text = 'Quandl WIKI Stock Prices - selected month (interactive legends: click to hide)'
+    p.legend.location = "top_left"
+    p.legend.click_policy = "hide"
+    p.xaxis.axis_label = 'Date [m/d]'
+    p.yaxis.axis_label = 'Daily stock price [$]'
+
+    script1, div1 = components(p)
+    cdn_js = CDN.js_files[0]
+    cdn_css = CDN.css_files[0]
+
+    return render_template('output_page.html', script1=script1, div1=div1, cdn_css=cdn_css, cdn_js=cdn_js, template="Flask")
 
 
 if __name__ == '__main__':
-  app.run(port=33507)
-  app.debug = True
+    app.run(debug=True)
+
--- app/tmp.py
+++ app/tmp.py
@@ -1,26 +1,76 @@
+# this file contains part of the code in app.py for debugging purpose
+
+from flask import Flask, render_template, request, redirect, flash, url_for
+from wtforms.widgets import ListWidget, CheckboxInput
+from flask_debugtoolbar import DebugToolbarExtension
 try: import simplejson as json
 except ImportError: import json
 import datetime
 import requests
 import pandas
 import io
-import bokeh
-#from bokeh.plotting import figure
-#from bokeh.resources import CDN
-#from bokeh.embed import file_html
+#import bokeh
+from bokeh.plotting import figure, output_file, show
+from bokeh.resources import CDN
+from bokeh.embed import components
+from wtforms import validators, StringField, BooleanField, SubmitField, widgets, SelectMultipleField
+from flask_wtf import FlaskForm, CSRFProtect
+from flask_nav import Nav
+from flask_nav.elements import Navbar, View, Subgroup, Link, Text, Separator
+from flask_bootstrap import Bootstrap
 
+import quandl
 
-ticker = 'FB'
-today = datetime.date(2018,1,1) #datetime.date.today()
+today = datetime.date(2018,2,1) #datetime.date.today()
 firstdaythismonth = today.replace(day=1)
 lastdaylastmonth = firstdaythismonth - datetime.timedelta(days=1)
 onemonthago = lastdaylastmonth.replace(day=today.day)
 
-payload = {'start_date' : onemonthago, 'end_date': today, 'api_key': 'HZpg_ZpjP46cpex3eccE'}
-r = requests.get('https://www.quandl.com/api/v3/datasets/WIKI/' + ticker + '/data.csv', params=payload)
-df = pandas.read_csv(io.StringIO(r.text))
+#payload = {'start_date': onemonthago, 'end_date': today, 'api_key': 'HZpg_ZpjP46cpex3eccE'}
+#r = requests.get('https://www.quandl.com/api/v3/datasets/EOD/HD', params=payload)
+#df = pandas.read_json(io.StringIO(r.text))#, parse_dates=['Date'])
 
+#ticker='FB'
+#quandl.ApiConfig.api_key = 'HZpg_ZpjP46cpex3eccE'
+#mydata = quandl.get('EOD/' + ticker, start_date=onemonthago, end_date=today)
 
-plot = bokeh.plotting.figure()
-plot.circle([1,2], [3,4])
-html = bokeh.embed.file_html(plot, bokeh.resources.CDN, "my plot")
+ticker='FB'
+interval_seconds = 86400
+num_days = 30
+
+payload = {'q': ticker, 'i': interval_seconds, 'p': str(num_days) + 'd', 'start_date': onemonthago, 'end_date': today, 'api_key': 'HZpg_ZpjP46cpex3eccE'}
+r = requests.get('http://www.google.com/finance/getprices', params=payload)
+
+df = pandas.read_csv(io.StringIO(r.text), skiprows=7, sep=',', names=['Date', 'Close', 'High', 'Low', 'Open', 'Volume'])
+df = df[df.Date != 'TIMEZONE_OFFSET=-240']
+b_dateround = df['Date'].map(lambda dt: dt[0]=='a')
+dateround = df[b_dateround]['Date'].map(lambda dt: int(dt[1:]))
+df['DATE2'] = dateround
+df['DATE2'] = df['DATE2'].fillna(method='ffill')
+df['DATE3'] = df[~b_dateround]['Date'].astype(int)*interval_seconds
+df['DATE3'] = df['DATE3'].fillna(0)
+df['DATE4'] = df['DATE2'] + df['DATE3']
+df['DATE4'] = df['DATE4'].map(lambda s: datetime.datetime.utcfromtimestamp(int(s)))
+del df['Date']
+del df['DATE2']
+del df['DATE3']
+df = df.set_index('DATE4', verify_integrity=True)
+df.index.name = 'Date'
+
+p = figure(x_axis_type="datetime", plot_width=700, plot_height=500)
+# p.line(x=df['Date'], y=df['Close'])
+
+lines=['Close', 'Open']
+colors = {'Close':'navy', 'Adj. Close': 'powderblue', 'Open': 'firebrick', 'Adj. Open':'salmon'}
+p = figure(x_axis_type="datetime", plot_width=700, plot_height=500)
+for line in lines:
+    p.line(x=df.index, y=df[line], color=colors[line])
+
+#p.title='Quandldata'
+output_file('tmp_plot.html')
+show(p)
+
+#plot = figure()
+#plot.line(x=df.Date, y=df.Close)
+# htmlpage = bokeh.embed.file_html(plot, bokeh.resources.CDN, "my plot")
+
